65499baf6c881862f73540de120c98004c47673d
2022148023 <m0o01@yonsei.ac.kr>
2023-05-14 20:19:30 +0900

    fix views

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ require("dotenv").config();
 const app = express();
 
 // set template engine to ejs
+app.set("views", "./views");
 app.set("view engine", "ejs");
 
 // serve static files located in public directory
 app.use(express.static("public"));
 
-// about page
+// main page
 app.get("/", function (req, res) {
-  res.render("home.ejs", {
+  res.render("home", {
     KAKAO_MAP_JAVASCRIPT_KEY:
       process.env.KAKAO_MAP_JAVASCRIPT_KEY ||
       "64e36d7df07a48189e25336dc4137d96",
