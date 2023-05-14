6d692a0c45d859625958103ac8bc4c761c9f920e
2022148023 <m0o01@yonsei.ac.kr>
2023-05-14 19:40:22 +0900

    inital commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+require("dotenv").config();
+
+// initialize new express app
+const app = express();
+
+// set template engine to ejs
+app.set("view engine", "ejs");
+
+// serve static files located in public directory
+app.use(express.static("public"));
+
+// about page
+app.get("/", function (req, res) {
+  res.render("home", {
+    KAKAO_MAP_JAVASCRIPT_KEY:
+      process.env.KAKAO_MAP_JAVASCRIPT_KEY ||
+      "64e36d7df07a48189e25336dc4137d96",
+  });
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, (err) => {
+  if (!err) {
+    console.log(`Server started at http://localhost:${port}`);
+  }
+});
