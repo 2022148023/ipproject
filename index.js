79d0fc9d463cb0469ef4cbf8576426b87b78ec7a
2022148023 <m0o01@yonsei.ac.kr>
2023-05-14 20:10:59 +0900

    vercel config

--- index.js
+++ index.js
@@ -21,8 +21,6 @@ app.get("/", function (req, res) {
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, (err) => {
-  if (!err) {
-    console.log(`Server started at http://localhost:${port}`);
-  }
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`);
 });
